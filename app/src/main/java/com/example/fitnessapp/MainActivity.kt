073e0018fb3228b98fbfaaa8d6e524a395d7725e
Vitalik-M07 <vitalik.mahovik@gmail.com>
2023-12-13 14:30:26 +0200

    13/12/23 С Ноута сделал 2-3 уроки

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.fitnessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fitnessapp.fragments.DaysFragment
+import com.example.fitnessapp.utils.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FragmentManager.setFragment(DaysFragment.newInstance(), this)
     }
 }
---
+++ app/src/main/java/com/example/fitnessapp/utils/FragmentManager.kt
@@ -0,0 +1,14 @@
+package com.example.fitnessapp.utils
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.fitnessapp.R
+
+object FragmentManager {
+    fun setFragment(newFragment: Fragment, act: AppCompatActivity){
+
+        val transaction = act.supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.placeHolder, newFragment)
+        transaction.commit()
+    }
+}
